6659f88350625f4667d9a23cc1b9660ed8793b38
vinipai45 <vinipai45@gmail.com>
2026-01-04 16:57:32 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+// IMPORTANT: service name = postgres
+const pool = new Pool({
+	host: "postgres",
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	port: 5432,
+});
+
+app.get("/", async (req, res) => {
+	const result = await pool.query("SELECT NOW()");
+	res.json({ time: result.rows[0] });
+});
+
+app.get("/users", async (req, res) => {
+	const result = await pool.query("SELECT * FROM users");
+	res.json({ users: result.rows });
+});
+
+app.listen(3000, () => {
+	console.log("Express running on port 3000");
+});
